0e9601617b9cf7c0501680d7b2a0040a0811e57e
maylenepoulsen <mayleneski@hotmail.com>
2020-06-03 17:40:54 -0500

    fixed else if on datefact request

--- src/utils/dateFact.js
+++ src/utils/dateFact.js
@@ -16,10 +16,12 @@ const dateFact = ((callback) => {
   }
 
   request(options, (error, { body }) => {
+    console.log(error)
+    console.log(body.text)
     if (error) {
         callback('Unable to connect to the Numbers api', undefined)
-    } else if (body.body === undefined) {
-        callback('Unable to connect to the Numbers api', undefined)
+     } else if (body.text === undefined) {
+         callback('Unable to connect to the Numbers api', undefined)
     } else {
         callback(undefined, {
           fact: body.text  
